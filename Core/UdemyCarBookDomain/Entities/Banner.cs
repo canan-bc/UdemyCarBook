6a6885f8b0e3ba126b4527447ca2dfe6e39c6c75
Canan <useryedek56@gmail.com>
2025-11-22 01:34:11 +0300

    Add project files.

---
+++ Core/UdemyCarBookDomain/Entities/Banner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBookDomain.Entities
+{
+    public class Banner
+    {
+        public int BannerID {  get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoUrl { get; set; }
+
+
+
+        //KENDİME NOT
+        /* ilk olarak entityleri tanımlıyoruz */
+
+
+    }
+}
